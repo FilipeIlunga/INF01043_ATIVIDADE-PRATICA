6768933e5d1bb8a609e9ab77c13975a15927bb2d
Filipe Ilunga <Filipeilunga@gmail.com>
2022-12-12 22:58:34 -0300

    [Atividade 1 - app 1] - Criando view

--- Atividade prática 1  - App 1/Atividade prática 1  - App 1/ContentView.swift
+++ Atividade prática 1  - App 1/Atividade prática 1  - App 1/ContentView.swift
@@ -8,19 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: String = ""
+    @State var number2: String = ""
+    @State var buttonDidTapped: Bool = false
+ 
+    @State var sum: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+                Form {
+                    Section {
+                        TextField("Enter a number", text: $number1)
+                            .keyboardType(.numberPad)
+                            .onChange(of: number1) { newValue in
+                                buttonDidTapped = false
+                            }
+                        TextField("Enter a number", text: $number2)
+                            .keyboardType(.numberPad)
+                            .onChange(of: number2) { newValue in
+                                buttonDidTapped = false
+                            }
+
+                        HStack {
+                            Spacer()
+                            Button("SUM") {
+                                buttonDidTapped = true
+                               
+                            }
+                            Spacer()
+                        }.listRowBackground(Color.gray.opacity(0.5))
+                    }
+                    
+                    Section {
+                        Text("Result: ")
+                    }
+                      
+                }.navigationTitle("Atividade 1")
         }
-        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(number1: "", number2: "")
     }
 }
+
