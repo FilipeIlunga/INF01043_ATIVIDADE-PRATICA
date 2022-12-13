a2c0d9936faf0768c803d0b4add0a1515d55eb50
Filipe Ilunga <Filipeilunga@gmail.com>
2022-12-12 23:21:15 -0300

    [Atividade 1 - app 3] - Criando projeto Swiftui

---
+++ Atividade prática 1  - App 3/Atividade prática 1  - App 3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Atividade prática 1  - App 3
+//
+//  Created by Filipe Xindanhi on 12/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
