e93d19b0c682fc1bdceaf937d2cc7aaa63f99fb4
Filipe Ilunga <Filipeilunga@gmail.com>
2022-12-12 23:13:23 -0300

    [Atividade 1 - app 2] - Criando view e enviando msg para outra tela

--- Atividade prática 1  - App 2/Atividade prática 1  - App 2/ContentView.swift
+++ Atividade prática 1  - App 2/Atividade prática 1  - App 2/ContentView.swift
@@ -8,19 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message: String = ""
+    @State var buttonDidTapped: Bool = false
+    @State var answer = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            Form {
+                Section {
+                    HStack {
+                        TextField("Enter a Message", text: $message)
+                        
+                        Divider()
+                        Button("SEND") {
+                            buttonDidTapped = true
+                        }
+                    }
+                } footer: {
+                    if buttonDidTapped {
+                        if message.isEmpty {
+                            Text("Put some message")
+                        } else {
+                            Text("Message sent to another view")
+                        }
+                    } else {
+                        Text("")
+                    }
+                }
+                if buttonDidTapped {
+                    NavigationLink(destination: View2(message: message), isActive: $answer, label: {
+                        Button(action: {
+                            
+                            answer = true
+                        }, label: {
+                            Text("Go to other view")
+                        })
+                    })
+                }
+            } .navigationTitle("Atividade 2")
+                .onAppear {
+                    buttonDidTapped = false
+                }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct View2: View {
+    var message: String
+    
+    var body: some View {
+        Text(message)
     }
 }
+
